2429d15677a75019be3e91fcfb100df23b7ed9c9
Administrator <my.jiangxiuhan@gmail.com>
2018-05-27 05:45:48 +0800

    Initial commit

---
+++ Web/ClientApp/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ElModule} from "element-angular/release/element-angular.module";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ElModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
